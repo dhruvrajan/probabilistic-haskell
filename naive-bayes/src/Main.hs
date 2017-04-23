5c6a456bbdca46dc1ffb37654e691bef05bd4b13
Dhruv Rajan <dhruv@krishnaprem.com>
2017-04-23 15:46:21 -0500

    separate by and add to class

--- naive-bayes/src/Main.hs
+++ naive-bayes/src/Main.hs
@@ -3,6 +3,7 @@ import Text.ParserCombinators.Parsec
 import Text.CSV
 import Data.Either
 import Data.Maybe
+import qualified Data.Map as Map
 
 
 -- Statistics Helper Functions
@@ -16,12 +17,36 @@ stdev ns = result
     variance = (sum [(n - avg)^2 | n <- ns]) / (fromIntegral (length ns - 1) :: Float)
     result = sqrt variance
 
+
+-- data transformations
 stringToFloat :: String -> Float
 stringToFloat s = read s :: Float
 
-getClassVecs :: CSV -> [([Float], Float)]
+getClassVecs :: CSV -> [(Int, [Float])]
 getClassVecs [] = []
-getClassVecs (x:xs) = (map stringToFloat $ take (length x - 1) x, stringToFloat $ last x) : getClassVecs xs
+getClassVecs (x:xs) = (read $ last x :: Int, map stringToFloat $ take (length x - 1) x) : getClassVecs xs
+
+
+addToClass :: (Int, [Float]) -> [(Int, [[Float]])] -> [(Int, [[Float]])]
+addToClass (x, ys) [] = [(x, [ys])]
+addToClass (x, ys) ((z1,z2) : zs) = if (x == z1)
+                                    then (z1, ys : z2) : zs
+                                    else (z1,  z2) : (addToClass (x, ys) zs)
+                                                 
+    
+
+separateByClass :: [(Int, [Float])] -> [(Int, [[Float]])] -> [(Int, [[Float]])]
+separateByClass [] ys = ys
+separateByClass (x:xs) ys = separateByClass xs (addToClass x ys)
+
+
+getCSVData :: IO([Record])
+getCSVData = do d <- parseCSVFromFile "data/csv_test.csv"
+                let y = head (rights [d])
+                return $ take (length y - 1) y
+
+
+
 
 processCSV :: CSV -> Bool
 processCSV x = True
