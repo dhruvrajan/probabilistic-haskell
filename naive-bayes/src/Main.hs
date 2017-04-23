8174a638054975697b4e4340866240755719741c
Dhruv Rajan <dhruv@krishnaprem.com>
2017-04-23 15:09:08 -0500

    preliminary stat helpers

--- naive-bayes/src/Main.hs
+++ naive-bayes/src/Main.hs
@@ -5,14 +5,30 @@ import Data.Either
 import Data.Maybe
 
 
+-- Statistics Helper Functions
+mean :: [Float] -> Float
+mean ns = (sum ns) / (fromIntegral (length ns) :: Float)
 
+stdev :: [Float] -> Float
+stdev ns = result
+  where
+    avg = mean(ns)
+    variance = (sum [(n - avg)^2 | n <- ns]) / (fromIntegral (length ns - 1) :: Float)
+    result = sqrt variance
 
-process :: (Either ParseError CSV) -> Maybe CSV;
-process (Right csv) = Just csv
-process (Left _) = Nothing
+stringToFloat :: String -> Float
+stringToFloat s = read s :: Float
+
+getClassVecs :: CSV -> [([Float], Float)]
+getClassVecs [] = []
+getClassVecs (x:xs) = (map stringToFloat $ take (length x - 1) x, stringToFloat $ last x) : getClassVecs xs
+
+processCSV :: CSV -> Bool
+processCSV x = True
+
 
 main :: IO ()
-main = do x1 <- parseCSVFromFile "../data/csv_test.csv"
-          let valid = process x1
-          putStrLn (if isJust valid then "True" else "False")
+main = do x1 <- parseCSVFromFile "data/csv_test.csv"
+          let result = (either (\x ->  False) (processCSV))  x1
+          putStrLn (if result then "True" else "False")
 
