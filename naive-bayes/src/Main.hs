691abfb09b5be1521f323c0d35c1c2337e9e9395
Dhruv Rajan <dhruv@krishnaprem.com>
2017-04-23 16:48:57 -0500

    more data files and probability calculation

--- naive-bayes/src/Main.hs
+++ naive-bayes/src/Main.hs
@@ -1,3 +1,4 @@
+
 module Main where
 import Text.ParserCombinators.Parsec
 import Text.CSV
@@ -14,17 +15,25 @@ stdev :: [Float] -> Float
 stdev ns = result
   where
     avg = mean(ns)
-    variance = (sum [(n - avg)^2 | n <- ns]) / (fromIntegral (length ns - 1) :: Float)
+    variance = (sum [(n - avg)^2 | n <- ns]) / (fromIntegral (if length ns - 1 == 0 then 1 else length ns - 1) :: Float)
     result = sqrt variance
-
+calcProb :: Float -> Float -> Float -> Float
+calcProb x m s = result
+  where
+    e = exp (-((x-m)^2)/(2 * (s^2)))
+    result = (1 / (sqrt(2*pi) * s)) * e
 
 -- data transformations
 stringToFloat :: String -> Float
 stringToFloat s = read s :: Float
 
-getClassVecs :: CSV -> [(Int, [Float])]
+getNumericData :: CSV -> [[Float]]
+getNumericData [] = []
+getNumericData (x:xs) = map stringToFloat x : getNumericData xs
+
+getClassVecs :: [[Float]] -> [(Int, [Float])]
 getClassVecs [] = []
-getClassVecs (x:xs) = (read $ last x :: Int, map stringToFloat $ take (length x - 1) x) : getClassVecs xs
+getClassVecs (x:xs) = (round (last x), take (length x - 1) x) : getClassVecs xs
 
 
 addToClass :: (Int, [Float]) -> [(Int, [[Float]])] -> [(Int, [[Float]])]
@@ -34,20 +43,38 @@ addToClass (x, ys) ((z1,z2) : zs) = if (x == z1)
                                     else (z1,  z2) : (addToClass (x, ys) zs)
                                                  
     
-
 separateByClass :: [(Int, [Float])] -> [(Int, [[Float]])] -> [(Int, [[Float]])]
 separateByClass [] ys = ys
 separateByClass (x:xs) ys = separateByClass xs (addToClass x ys)
 
 
 getCSVData :: IO([Record])
-getCSVData = do d <- parseCSVFromFile "data/csv_test.csv"
+getCSVData = do d <- parseCSVFromFile "data/diabetes.csv"
                 let y = head (rights [d])
                 return $ take (length y - 1) y
 
+transpose :: [Float] -> [[Float]]
+transpose [] = []
+transpose (x:xs) = [x] : transpose xs
 
+zipWithCons :: [[Float]] -> [[Float]]
+zipWithCons [] = []
+zipWithCons [x] = transpose x
+zipWithCons (x1 : xs) = zipWith (:) x1 (zipWithCons xs)
 
+summarizeData :: [[Float]] -> [(Float, Float)]
+summarizeData xs = result
+  where
+    zipped = zipWithCons xs
+    result = [(mean row, stdev row) | row <- zipped]
 
+summarizeByClass :: [[Float]] -> [(Int, [(Float, Float)])]
+summarizeByClass xs = result
+  where 
+    byClass = separateByClass (getClassVecs xs) []
+    result = map (\(x1,x2) -> (x1, summarizeData x2)) byClass
+
+    
 processCSV :: CSV -> Bool
 processCSV x = True
 
