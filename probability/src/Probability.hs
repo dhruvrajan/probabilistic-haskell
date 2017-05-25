669f21eebd82224f864755d1ffe19dc03464399c
Dhruv Rajan <dhruv@krishnaprem.com>
2017-05-25 16:00:01 -0700

    added planning latex file

--- probability/src/Probability.hs
+++ probability/src/Probability.hs
@@ -1,7 +1,26 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Probability
   (
-    someFunc
+    someFunc,
+    ProbDist    
   ) where
+
+-- Generic Probability Distribution
+-- Maps values to probabilities.
+-- Need to create Random Variables
+-- Random variables can be Discrete, Continuous
+data ProbDist a = ProbDist { }
+
+newtype Normal = Normal mu sigma
+
+
+class (Num t) => Discrete t where
+  mode :: ProbDist t -> Float
+  
+
+class (Num t) => Continuous t where
+  mode :: ProbDist t -> Float
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
