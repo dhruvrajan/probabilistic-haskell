6e62c20b3e0cfbac60c0659f7db29b69cd7e9115
Dhruv Rajan <dhruv@krishnaprem.com>
2017-06-09 01:20:07 -0700

    finished hello world implementation

--- probability/app/Main.hs
+++ probability/app/Main.hs
@@ -1,6 +1,36 @@
 module Main where
 
-import Probability
+import qualified Probability as P
+
+select1 :: P.Distribution String
+select1 "Hello, world!" = 0.6
+select1 "Howdy, universe!" = 0.4
+
+select2 :: P.Distribution String
+select2 "Hello, world!" = 0.2
+select2 "Howdy, universe!" = 0.8
 
 main :: IO ()
-main = someFunc
+main = do
+  -- Set Up
+  let sunnyToday = P.flip 0.2
+  let greetingToday1 = P.If sunnyToday (P.fromDistribution select1)
+                          (P.fromDistribution select2)
+  let sunnyTomorrow = P.If sunnyToday (P.flip 0.8) (P.flip 0.05)
+  let greetingTomorrow = P.If sunnyTomorrow (P.fromDistribution select1)
+                         (P.fromDistribution select2)
+
+  let prediction = P.probability greetingToday1 "Hello, world!"
+  putStrLn("Today's greeting is \"Hello, world!\" " ++
+           "with probability " ++ (show prediction) ++ ".")
+
+  let greetingToday2 = P.observe greetingToday1 "Hello, world!"
+  let inference = P.probability sunnyToday True
+  putStrLn ("If today's greeting is \"Hello, world!\", today's " ++
+          "weather is sunny with probability " ++ (show inference) ++ ".")
+
+  let greetingToday3 = P.observe greetingToday2 "Hello, world!"
+  let result = P.probability greetingTomorrow "Hello, world!"
+  putStrLn("If today's greeting is\"Hello, world!\", " ++
+          "tomorrow's greeeting will be \"Hello, world!\" " ++
+          "with probability " ++ (show result) ++ ".")
--- probability/src/Probability.hs
+++ probability/src/Probability.hs
@@ -2,10 +2,17 @@ module Probability where
 
 type Distribution a = a -> Double
 
-data Element a = Element { distribution :: Distribution a,
-                           observed :: Maybe a
-                         }
+data Element a = Atomic { distribution :: Distribution a, observed :: Maybe a}
+  | If {tst :: Element Bool, thn :: Element a, els :: Element a}
 
+fromDistribution :: Distribution a -> Element a
+fromDistribution d = Atomic {distribution = d, observed = Nothing}
+
+flip :: Double -> Element Bool
+flip p = fromDistribution $ \x  -> case x of True ->  p
+                                             False -> 1 - p
+
+         
 observe :: Element a -> a -> Element a
 observe e value = e { observed = Just value }
 
@@ -13,9 +20,12 @@ unobserve :: Element a -> Element a
 unobserve e = e { observed = Nothing }
 
 probability :: Eq a => Element a -> a -> Double
-probability Element { distribution = d, observed = Nothing } = d
-probability Element { distribution = d, observed = Just val}
-  = \x -> if (x == val) then 1.0 else 0.0
+probability Atomic { distribution = d, observed = Nothing } val = d val
+probability Atomic { distribution = d, observed = Just val} x
+  = if (x == val) then 1.0 else 0.0
+probability If {tst = tst, thn = thn, els = els} val =
+  (probability tst True) * (probability thn val) +
+  (probability tst False) * (probability els val)
 
 
 dist :: Distribution Bool
