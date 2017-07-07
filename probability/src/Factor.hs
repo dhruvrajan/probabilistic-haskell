5a85847f02c339a074f8dc45d8d732c7929aa303
Dhruv Rajan <dhruv@krishnaprem.com>
2017-07-07 08:14:27 -0700

    looking at implementing Figaro's "factor" dataype

---
+++ probability/src/Factor.hs
@@ -0,0 +1,9 @@
+module Factor where
+
+import Data.Map
+-- A factor is a function from the value of a set of variables to a
+-- real number
+
+-- Conditional Probability Table (Discrete Factor)
+type CPT = Map ()
+
