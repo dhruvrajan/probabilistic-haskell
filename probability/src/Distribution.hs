d9dc81756d6f7785738e2758382a31d162b7bf2e
Dhruv Rajan <dhruv@krishnaprem.com>
2017-07-07 08:17:40 -0700

    standard distribution types

--- probability/src/Distribution.hs
+++ probability/src/Distribution.hs
@@ -2,19 +2,23 @@ module Distribution where
 import Data.List (lookup)
 import Data.Maybe
 
-type Distribution a = a -> Double
+data Distribution a = Continuous (a -> Double) | Discrete [(a, Double)]
 
 (->>) :: a -> b -> (a, b)
 x ->> y = (x, y)
 
 bernoulli :: Double -> Distribution Bool
-bernoulli p = \x -> case x of True -> p
-                              False -> 1 - p
+bernoulli p = Discrete [(True, p), (False, 1-p)]
+
 constant :: Eq a => a -> Distribution a
-constant val = \x -> if x == val then 1.0 else 0.0
+constant val = Discrete [(val, 1.0)]
 
 select :: Eq a => [(a, Double)] -> Distribution a
-select pairs =
+select pairs = Discrete pairs
+
+pdf :: Eq a =>  Distribution a -> (a -> Double)
+pdf (Continuous fn) = fn
+pdf (Discrete pairs) =
   \x ->
     if sum [snd y | y <- pairs] == 1.0
     then (\x -> if isJust x then fromJust x else 0.0) $ lookup x pairs
