fb0308d8341359b3cf20bc390bc4483a8c31f777
Dhruv Rajan <dhruv@krishnaprem.com>
2017-07-24 09:52:26 -0700

    working on a linear regression implementation in Figaro

---
+++ figaro/linear-regression/build.sbt
@@ -0,0 +1,9 @@
+name := "Linear Regression"
+
+version := "1.0"
+
+scalaVersion := "2.11.11"
+
+libraryDependencies ++= Seq(
+  "com.cra.figaro" % "figaro_2.11" % "4.1.0.0"
+)
---
+++ figaro/linear-regression/src/main/scala/regression/Regression.scala
@@ -0,0 +1,38 @@
+package regression
+
+
+import com.cra.figaro.algorithm.learning.EMWithBP
+import com.cra.figaro.language.Flip
+import com.cra.figaro.library.atomic.continuous.Beta
+
+object Regression extends App {
+  def toss(prob: Double, n: Int): Seq[Boolean] = {
+    val coin = Flip(prob)
+    for (_ <- 1 to n) yield {
+      coin.generate()
+      coin.value
+    }
+  }
+
+  // First get a lot of tosses (learning data).
+  val probability = 0.5
+  val tosses = toss(probability, 1000)
+
+  // Parameter modeling the bias in our coin.  (Apparently the conjugate prior
+  // for a Bernoulli: Flip is a Beta distribution.)
+  val fairness = Beta(1, 1)
+
+  // Construct an element for each trial; observe() with the value from tosses.
+  val trials = for (t <- tosses) yield {
+    val trial = Flip(fairness)
+    trial.observe(t)
+    trial
+  }
+
+  // Instantiate the learning algorithm, and run it.
+  val algorithm = EMWithBP(fairness)
+  algorithm.start()
+  algorithm.kill()
+
+  println(s"Actual: $probability learned: ${fairness.MAPValue}")
+}
