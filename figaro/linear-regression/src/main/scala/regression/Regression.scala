51ff678a3d17bdc178442fc3e185d21f9ea2c172
Dhruv Rajan <dhruv@krishnaprem.com>
2017-07-24 15:30:46 -0700

    linear regression pipeline finished; still has errors

--- figaro/linear-regression/src/main/scala/regression/Regression.scala
+++ figaro/linear-regression/src/main/scala/regression/Regression.scala
@@ -2,8 +2,9 @@ package regression
 
 
 
+import coinflip.CoinFlip.{fairness, probability}
 import com.cra.figaro.algorithm.learning.EMWithBP
-import com.cra.figaro.language.Flip
+import com.cra.figaro.language.{Apply, Chain, Flip}
 import com.cra.figaro.library.atomic.continuous.{AtomicNormal, Beta, Normal, Uniform}
 
 object Regression extends App {
@@ -14,9 +15,11 @@ object Regression extends App {
     * @param beta  constant term
     * @param sigma standard deviation for gaussian noise (mean is 0)
     */
-  def dataset(alpha: Double, beta: Double, sigma: Double, size: Int): Seq[(Double, Double)] = {
+  def dataset(alpha: Double, beta: Double,
+              sigma: Double, size: Int) : Seq[(Double, Double)] = {
     val x = Normal(0, 1)
     val noise = Normal(0, sigma)
+    var xMean = 0.0
     for (_ <- 1 to size) yield {
       x.generate()
       noise.generate()
@@ -24,6 +27,28 @@ object Regression extends App {
     }
   }
 
-  val alpha = 0.5
-  val beta = 1
+  val data = dataset(2, 2, 1, 100)
+
+  val alpha = Beta(0, 10)
+  val beta = Beta(0, 10)
+
+  val trials = for ((x, y) <- data) yield {
+    val predictor = Normal(0, 1)
+    val mu = Apply(alpha, beta, predictor, (a: Double, b: Double, x: Double) => a * x + b)
+
+    predictor.observe(x)
+    mu.observe(y)
+    (predictor, mu)
+  }
+
+  var algorithm = EMWithBP(alpha)
+  algorithm.start()
+  algorithm.kill()
+
+  algorithm = EMWithBP(beta)
+  algorithm.start()
+  algorithm.kill()
+
+  println(s"Alpha: 2 learned: ${alpha.MAPValue}")
+  println(s"Beta: 2 learned ${beta.MAPValue}")
 }
