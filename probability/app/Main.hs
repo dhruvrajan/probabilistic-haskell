8974b22b2db6f30c551001dd9c30eff64a8b4d21
Dhruv Rajan <dhruv@krishnaprem.com>
2017-07-24 09:52:41 -0700

    working on fixing the RV - Element implementation

--- probability/app/Main.hs
+++ probability/app/Main.hs
@@ -1,114 +1,116 @@
 module Main where
 
-import Data.Monoid
-import Control.Monad.Writer
-import Control.Monad.State
+-- import Data.Monoid
+-- import Control.Monad.Writer
+-- import Control.Monad.State
 
-type Stack = [Int]
+-- type Stack = [Int]
 
-pop' :: Stack -> (Int, Stack)
-pop' (x:xs) = (x,xs)
+-- pop' :: Stack -> (Int, Stack)
+-- pop' (x:xs) = (x,xs)
 
-push' :: Int -> Stack -> ((), Stack)
-push' a xs = ((), a:xs)
+-- push' :: Int -> Stack -> ((), Stack)
+-- push' a xs = ((), a:xs)
 
-stackManip' :: Stack -> (Int, Stack)
-stackManip' stack = let
-  ((), newStack1) = push' 3 stack
-  (a, newStack2) = pop' newStack1
-  in pop' newStack2
+-- stackManip' :: Stack -> (Int, Stack)
+-- stackManip' stack = let
+--   ((), newStack1) = push' 3 stack
+--   (a, newStack2) = pop' newStack1
+--   in pop' newStack2
 
-pop :: State Stack Int
-pop = state $ \(x:xs) -> (x, xs)
+-- pop :: State Stack Int
+-- pop = state $ \(x:xs) -> (x, xs)
 
-push :: Int -> State Stack ()
-push a = state $ \xs -> ((), a:xs)
+-- push :: Int -> State Stack ()
+-- push a = state $ \xs -> ((), a:xs)
 
-stackManip :: State Stack Int
-stackManip = do
-  push 3
-  a <- pop
-  pop 
-
-
-newtype DiffList a = DiffList {getDiffList :: [a] -> [a]}
-
-toDiffList :: [a] -> DiffList a
-toDiffList xs = DiffList (xs++)
-
-fromDiffList :: DiffList a -> [a]
-fromDiffList (DiffList f) = f []
-
-instance Monoid (DiffList a) where
-  mempty = DiffList (\xs -> xs)
-  (DiffList f) `mappend` (DiffList g) = DiffList (\xs -> f . g $ xs)
+-- stackManip :: State Stack Int
+-- stackManip = do
+--   push 3
+--   a <- pop
+--   pop 
 
 
-logNumber :: Int -> Writer [String] Int
-logNumber x = writer (x, ["Got number: " ++ show x])
+-- newtype DiffList a = DiffList {getDiffList :: [a] -> [a]}
 
-multWithLog :: Writer [String] Int
-multWithLog = do
-  a <- logNumber 3
-  b <- logNumber 5
-  tell ["Gonna multiply now"]
-  return (a*b)
+-- toDiffList :: [a] -> DiffList a
+-- toDiffList xs = DiffList (xs++)
 
-isBigGang :: Int -> (Bool, [String])
-isBigGang x = (x > 9, ["Compared gang size to 9."])
+-- fromDiffList :: DiffList a -> [a]
+-- fromDiffList (DiffList f) = f []
 
-type Food = String
-type Price = Sum Int
-
-addDrink :: Food -> (Food, Price)
-addDrink "beans" = ("milk", Sum 25)
-addDrink "jerky" = ("whiskey", Sum 99)
-addDrink _ = ("beer", Sum 30)
+-- instance Monoid (DiffList a) where
+--   mempty = DiffList (\xs -> xs)
+--   (DiffList f) `mappend` (DiffList g) = DiffList (\xs -> f . g $ xs)
 
 
-applyLog :: Monoid m => (a, m) -> (a -> (b, m)) -> (b, m)
-applyLog (x, log) f = let (y, newLog) = f x in (y, log `mappend` newLog)
+-- logNumber :: Int -> Writer [String] Int
+-- logNumber x = writer (x, ["Got number: " ++ show x])
+
+-- multWithLog :: Writer [String] Int
+-- multWithLog = do
+--   a <- logNumber 3
+--   b <- logNumber 5
+--   tell ["Gonna multiply now"]
+--   return (a*b)
+
+-- isBigGang :: Int -> (Bool, [String])
+-- isBigGang x = (x > 9, ["Compared gang size to 9."])
+
+-- type Food = String
+-- type Price = Sum Int
+
+-- addDrink :: Food -> (Food, Price)
+-- addDrink "beans" = ("milk", Sum 25)
+-- addDrink "jerky" = ("whiskey", Sum 99)
+-- addDrink _ = ("beer", Sum 30)
+
+
+-- applyLog :: Monoid m => (a, m) -> (a -> (b, m)) -> (b, m)
+-- applyLog (x, log) f = let (y, newLog) = f x in (y, log `mappend` newLog)
+
+-- main :: IO ()
+-- main = do
+--   putStrLn . show $ isBigGang 3
+
+
+
+
+import Distribution
+import Element
+import Inference
+
+select1 :: Distribution String
+select1 "Hello, world!" = 0.6
+select1 "Howdy, universe!" = 0.4
+
+select2 :: Distribution String
+select2 "Hello, world!" = 0.2
+select2 "Howdy, universe!" = 0.8
 
 main :: IO ()
-main = do
-  putStrLn . show $ isBigGang 3
+main  = do
+  -- Set Up
+  let sunnyToday = flip 0.2
+  let greetingToday1 = If sunnyToday (fromDistribution select1)
+                          (fromDistribution select2) Nothing
+  let sunnyTomorrow = If sunnyToday (flip 0.8) (flip 0.05) Nothing
+  let greetingTomorrow = If sunnyTomorrow (fromDistribution select1)
+                         (fromDistribution select2) Nothing
+  -- Predict
+  let prediction = probability greetingToday1 "Hello, world!"
+  putStrLn("Today's greeting is \"Hello, world!\" " ++
+           "with probability " ++ (show prediction) ++ ".")
 
+  -- Infer
+  let greetingToday2 = observe greetingToday1 "Hello, world!"
+  let inference = probability sunnyToday True
+  putStrLn ("If today's greeting is \"Hello, world!\", today's " ++
+          "weather is sunny with probability " ++ (show inference) ++ ".")
 
--- module Main where
-
--- import qualified Probability as P
-
--- select1 :: P.Distribution String
--- select1 "Hello, world!" = 0.6
--- select1 "Howdy, universe!" = 0.4
-
--- select2 :: P.Distribution String
--- select2 "Hello, world!" = 0.2
--- select2 "Howdy, universe!" = 0.8
-
--- main1 :: IO ()
--- main1 = do
---   -- Set Up
---   let sunnyToday = P.flip 0.2
---   let greetingToday1 = P.If sunnyToday (P.fromDistribution select1)
---                           (P.fromDistribution select2) Nothing
---   let sunnyTomorrow = P.If sunnyToday (P.flip 0.8) (P.flip 0.05) Nothing
---   let greetingTomorrow = P.If sunnyTomorrow (P.fromDistribution select1)
---                          (P.fromDistribution select2) Nothing
---   -- Predict
---   let prediction = P.probability greetingToday1 "Hello, world!"
---   putStrLn("Today's greeting is \"Hello, world!\" " ++
---            "with probability " ++ (show prediction) ++ ".")
-
---   -- Infer
---   let greetingToday2 = P.observe greetingToday1 "Hello, world!"
---   let inference = P.probability sunnyToday True
---   putStrLn ("If today's greeting is \"Hello, world!\", today's " ++
---           "weather is sunny with probability " ++ (show inference) ++ ".")
-
---   -- Learn And Predict
---   let greetingToday3 = P.observe greetingToday2 "Hello, world!"
---   let result = P.probability greetingTomorrow "Hello, world!"
---   putStrLn("If today's greeting is\"Hello, world!\", " ++
---           "tomorrow's greeeting will be \"Hello, world!\" " ++
---           "with probability " ++ (show result) ++ ".")
+  -- Learn And Predict
+  let greetingToday3 = observe greetingToday2 "Hello, world!"
+  let result = probability greetingTomorrow "Hello, world!"
+  putStrLn("If today's greeting is\"Hello, world!\", " ++
+          "tomorrow's greeeting will be \"Hello, world!\" " ++
+          "with probability " ++ (show result) ++ ".")
--- probability/src/Inference.hs
+++ probability/src/Inference.hs
@@ -6,12 +6,12 @@ import Distribution
 
 --expectation :: Eq a => RV a -> a
 
-probability :: (Eq a) => RV a -> a -> Double
-probability (Unobserved (Atomic dist)) val = (pdf dist) val
-probability (Unobserved (ScalarAdd rv n)) val = probability rv (val - n)
-probability (Unobserved (ScalarProduct rv n)) val = probability rv (val / n)
-probability (Unobserved (RVProduct rv n)) val = 
---probability (Observed   (Atomic dist) obs) val = if (obs == val) then 1.0 else 0.0
+-- probability :: (Eq a) => RV a -> a -> Double
+-- probability (Unobserved (Atomic dist)) val = (pdf dist) val
+-- probability (Unobserved (ScalarAdd rv n)) val = probability rv (val - n)
+-- probability (Unobserved (ScalarProduct rv n)) val = probability rv (val / n)
+-- probability (Unobserved (RVProduct rv n)) val = 
+-- --probability (Obser  ved   (Atomic dist) obs) val = if (obs == val) then 1.0 else 0.0
 
 
 
@@ -19,11 +19,12 @@ probability (Unobserved (RVProduct rv n)) val =
 -- probability (Observed (Atomic dist) val) = dist val
 -- probability ()
 
--- probability :: Eq a => Element a -> a -> Double
--- probability (Atomic dist ) val = dist val
--- probability (Atomic dist (Just x)) val = if (x == val) then 1.0 else 0.0
 
--- probability (If tst thn els Nothing) val =
---   (probability tst True) * (probability thn val) +
---   (probability tst False) * (probability els val)
+probability :: Eq a => RV a -> a -> Double
+probability (Unobserved (Atomic dist)) val = (pdf dist) val
+probability (Unobserved (If tst thn els)) val=
+  (probability tst True)  * (probability thn val) +
+  (probability tst False) * (probability els val)
+probability (Observed (_) x) val = if (x == val) then 1.0 else 0.0
+
 
