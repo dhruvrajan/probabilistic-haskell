e49e048dcd1fc858780d675cc1084d83dd63869a
Dhruv Rajan <dhruv@krishnaprem.com>
2017-07-24 10:42:57 -0700

    method to generate linear regression data

--- figaro/linear-regression/src/main/scala/regression/Regression.scala
+++ figaro/linear-regression/src/main/scala/regression/Regression.scala
@@ -1,38 +1,29 @@
 package regression
 
 
+
 import com.cra.figaro.algorithm.learning.EMWithBP
 import com.cra.figaro.language.Flip
-import com.cra.figaro.library.atomic.continuous.Beta
+import com.cra.figaro.library.atomic.continuous.{AtomicNormal, Beta, Normal, Uniform}
 
 object Regression extends App {
-  def toss(prob: Double, n: Int): Seq[Boolean] = {
-    val coin = Flip(prob)
-    for (_ <- 1 to n) yield {
-      coin.generate()
-      coin.value
+  /**
+    * Generate Data for Linear Regression, according to the model:
+    * Y = alpha * x + beta (+ gaussian noise)
+    * @param alpha coefficient of predictor x
+    * @param beta  constant term
+    * @param sigma standard deviation for gaussian noise (mean is 0)
+    */
+  def dataset(alpha: Double, beta: Double, sigma: Double, size: Int): Seq[(Double, Double)] = {
+    val x = Normal(0, 1)
+    val noise = Normal(0, sigma)
+    for (_ <- 1 to size) yield {
+      x.generate()
+      noise.generate()
+      (x.value, alpha * x.value + beta + noise.value)
     }
   }
 
-  // First get a lot of tosses (learning data).
-  val probability = 0.5
-  val tosses = toss(probability, 1000)
-
-  // Parameter modeling the bias in our coin.  (Apparently the conjugate prior
-  // for a Bernoulli: Flip is a Beta distribution.)
-  val fairness = Beta(1, 1)
-
-  // Construct an element for each trial; observe() with the value from tosses.
-  val trials = for (t <- tosses) yield {
-    val trial = Flip(fairness)
-    trial.observe(t)
-    trial
-  }
-
-  // Instantiate the learning algorithm, and run it.
-  val algorithm = EMWithBP(fairness)
-  algorithm.start()
-  algorithm.kill()
-
-  println(s"Actual: $probability learned: ${fairness.MAPValue}")
+  val alpha = 0.5
+  val beta = 1
 }
