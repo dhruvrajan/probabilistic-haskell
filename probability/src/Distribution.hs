27e12ed14b35aaafb5de21f1f565989608fb3935
Dhruv Rajan <dhruv@krishnaprem.com>
2017-07-25 14:27:31 -0700

    new implementation for Normal distribution

--- probability/src/Distribution.hs
+++ probability/src/Distribution.hs
@@ -1,19 +1,23 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 module Distribution where
-import Data.List (lookup)
-import Data.Maybe
-  
+
+import qualified Statistics.Distribution as DistLib
+import qualified Statistics.Distribution.Normal as NormalLib
+
+
+-- Generic Distribution Classes  
 class Distribution a where
   sum :: a -> Double
   
 class Distribution a => Discrete a b where
-  -- Probability Mass Function
+  -- Probability Mass Function P(X=x)
   pmf :: a -> b -> Double
 
 class Distribution a =>  Continuous a where
+  -- Probability Density Function P(X in [x, dx])
   pdf :: a -> Double -> Double
+  -- Cumulative Distribution Function P(X < x)
   cdf :: a -> Double -> Double
-
 
 -- Bernoulli Distribution
 data Bernoulli = Bernoulli Double
@@ -26,3 +30,15 @@ instance Discrete Bernoulli Double where
 
 instance Discrete Bernoulli Bool where
   pmf (Bernoulli p) x = if (x == True) then p else 1 - p
+
+-- Normal Distribution
+data Normal = Normal Double Double
+
+instance Continuous Normal where
+  pdf (Normal mu sigma) = DistLib.density $ NormalLib.normalDistr mu sigma
+  cdf (Normal mu sigma) = DistLib.cumulative $ NormalLib.normalDistr mu sigma
+  
+instance Distribution Normal where
+  sum (Normal mu sigma) = (cdf (Normal mu sigma) mu) + (cdf (Normal mu sigma) mu)
+  
+  
