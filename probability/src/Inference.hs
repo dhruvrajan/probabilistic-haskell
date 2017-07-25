91f7c0ed67da3a55198d544eb1a14c799e747ba3
Dhruv Rajan <dhruv@krishnaprem.com>
2017-07-25 16:21:14 -0700

    incorporating new distribution changes to inference algorithm

--- probability/src/Inference.hs
+++ probability/src/Inference.hs
@@ -3,25 +3,9 @@ module Inference where
 import Element
 import Distribution
 
-
---expectation :: Eq a => RV a -> a
-
--- probability :: (Eq a) => RV a -> a -> Double
--- probability (Unobserved (Atomic dist)) val = (pdf dist) val
--- probability (Unobserved (ScalarAdd rv n)) val = probability rv (val - n)
--- probability (Unobserved (ScalarProduct rv n)) val = probability rv (val / n)
--- probability (Unobserved (RVProduct rv n)) val = 
--- --probability (Obser  ved   (Atomic dist) obs) val = if (obs == val) then 1.0 else 0.0
-
-
-
-
--- probability (Observed (Atomic dist) val) = dist val
--- probability ()
-
-
 probability :: Eq a => RV a -> a -> Double
-probability (Unobserved (Atomic dist)) val = (pdf dist) val
+probability (Unobserved (DiscreteAtomic dist)) val = pmf dist val
+probability (Unobserved (ContinuousAtomic dist)) val = pdf dist val
 probability (Unobserved (If tst thn els)) val=
   (probability tst True)  * (probability thn val) +
   (probability tst False) * (probability els val)
