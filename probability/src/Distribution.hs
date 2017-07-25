8457ceae88bd0905e8ca5c9be787717563fb3d48
Dhruv Rajan <dhruv@krishnaprem.com>
2017-07-25 16:18:23 -0700

    making continuous distributions have an extra type parameter (Double)

--- probability/src/Distribution.hs
+++ probability/src/Distribution.hs
@@ -1,29 +1,32 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module Distribution where
 
+import qualified Data.List as ListLib
 import qualified Statistics.Distribution as DistLib
 import qualified Statistics.Distribution.Normal as NormalLib
 
-
 -- Generic Distribution Classes  
 class Distribution a where
-  sum :: a -> Double
+  sumProbabilities :: a -> Double
   
 class Distribution a => Discrete a b where
   -- Probability Mass Function P(X=x)
   pmf :: a -> b -> Double
 
-class Distribution a =>  Continuous a where
+class Distribution a => Continuous a b where
   -- Probability Density Function P(X in [x, dx])
-  pdf :: a -> Double -> Double
+  pdf :: a -> b -> Double
   -- Cumulative Distribution Function P(X < x)
-  cdf :: a -> Double -> Double
+  cdf :: a  -> b -> Double
 
 -- Bernoulli Distribution
 data Bernoulli = Bernoulli Double
 
 instance Distribution Bernoulli where
-  sum (Bernoulli p) = p + (1 - p)
+  sumProbabilities (Bernoulli p) = p + (1 - p)
 
 instance Discrete Bernoulli Double where
   pmf (Bernoulli p) x = if (x == 1) then p else 1 - p
@@ -31,15 +34,25 @@ instance Discrete Bernoulli Double where
 instance Discrete Bernoulli Bool where
   pmf (Bernoulli p) x = if (x == True) then p else 1 - p
 
+-- Select Distribution
+data Select a  = Eq a => Select [(a, Double)]
 
+instance Distribution (Select a) where
+  sumProbabilities (Select []) = 0
+  sumProbabilities (Select ((_, p):pairs)) = p + sumProbabilities (Select pairs)
+
+instance Discrete (Select a) a where
+  pmf (Select pairs) x = case ListLib.lookup x pairs of
+                           (Just p) -> p
+                           Nothing -> 0                           
 -- Normal Distribution
 data Normal = Normal Double Double
 
-instance Continuous Normal where
+instance Continuous Normal Double where
   pdf (Normal mu sigma) = DistLib.density $ NormalLib.normalDistr mu sigma
   cdf (Normal mu sigma) = DistLib.cumulative $ NormalLib.normalDistr mu sigma
   
 instance Distribution Normal where
-  sum (Normal mu sigma) = (cdf (Normal mu sigma) mu) + (cdf (Normal mu sigma) mu)
+  sumProbabilities (Normal mu sigma) = (cdf (Normal mu sigma) mu) + (cdf (Normal mu sigma) mu)
   
   
