a1ce321aeead194f7ce5535f66bbd8444936c551
Dhruv Rajan <dhruv@krishnaprem.com>
2017-07-25 16:12:23 -0700

    separating discrete and continuous atomic variables

--- probability/src/Element.hs
+++ probability/src/Element.hs
@@ -1,50 +1,47 @@
 {-# LANGUAGE ExistentialQuantification #-}
 module Element where
 import Control.Monad
-import Data.Maybe
-import Data.List
 import Distribution
 
 data Element a =
-  forall d . Distribution d => Atomic d
+  forall d. Discrete d a => DiscreteAtomic d
+  | forall d. Continuous d => ContinuousAtomic d
   | If (RV Bool) (RV a) (RV a)
   | forall b. Apply (RV b) (b -> a)
   | forall b. Chain (RV b) (b -> a)
 
-data RV a = Observed (Element a) a | Unobserved (Element a)  
+data RV a = Observed (Element a) a | Unobserved (Element a)
 
--- instance Show a => Show (Element a) where
---   show (Atomic dist) = "Atomic"
---   show (Apply _ _) = "Apply"
---   show (Negate rv) = "Negate (" ++ (show rv)
---   show (ScalarAdd rv val) = "ScalarAdd (" ++ (show rv) ++ (show val) ++ ") "
---   show (Sum rv1 rv2) = "Sum ("  ++ (show rv1) ++ (show rv2) ++ ") "
---   show (RVProduct rv1 rv2) = "RVProduct (" ++ (show rv1) ++ (show rv2) ++ ") "
---   show (ScalarProduct rv n) = "ScalarProduct (" ++ (show rv) ++ (show n) ++ ") "
---   show (Power rv 
---        n) = "Power (" ++ (show rv) ++ (show n) ++ ") "
+observe :: RV a -> a -> RV a
+observe (Observed e _) val = Observed e val
+observe (Unobserved e) val = Observed e val
 
--- data RV a = Observed (Element a) a | Unobserved (Element a)
+unobserve :: RV a -> RV a
+unobserve (Observed e _) = Unobserved e
+unobserve (Unobserved e) = Unobserved e
 
--- instance Show a => Show (RV a) where
---   show (Observed e val) = "Observed" ++ (show e) ++ (show val)
---   show (Unobserved e) = "Unobserved (" ++ (show e) ++ ") "
+fromDiscrete :: Discrete d a => d -> RV a
+fromDiscrete dist = Unobserved $ DiscreteAtomic dist
+
+fromContinuous :: Continuous d => d -> RV a
+fromContinuous dist = Unobserved $ ContinuousAtomic dist
+
+instance Show a => Show (Element a) where
+  show (DiscreteAtomic dist) = "DiscreteAtomic"
+  show (ContinuousAtomic dist) = "ContinuousAtomic"
+  show (If _ _ _ ) = "If"
+  show (Apply _ _) = "Apply"
+  show (Chain _ _) = "Chain"
+
+instance Show a => Show (RV a) where
+  show (Observed e val) = "Observed" ++ (show e) ++ (show val)
+  show (Unobserved e) = "Unobserved (" ++ (show e) ++ ") "
   
 -- instance Num a => Num (RV a) where
 --   rv1 + rv2 = Unobserved $ Sum rv1 rv2
 --   rv1 * rv2 = Unobserved $ RVProduct rv1 rv2
 --   negate rv = Unobserved $ Negate rv
 
--- observe :: RV a -> a -> RV a
--- observe (Observed e _) val = Observed e val
--- observe (Unobserved e) val = Observed e val
-
--- unobserve :: RV a -> RV a
--- unobserve (Observed e _) = Unobserved e
--- unobserve (Unobserved e) = Unobserved e
-
--- fromDistribution :: Distribution a -> RV a
--- fromDistribution dist = Unobserved $ Atomic dist
 
 -- coinFlip :: Double -> RV Bool
 -- coinFlip p = fromDistribution $ bernoulli p
