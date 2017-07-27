c80e56ae941786f38ea7f55ed9e6ce2ea8fa7c67
Dhruv Rajan <dhruv@krishnaprem.com>
2017-07-26 20:45:39 -0700

    possible fix to distribution typeclassess

--- probability/src/Coin.hs
+++ probability/src/Coin.hs
@@ -6,13 +6,16 @@ data Coin = Head | Tail deriving (Eq, Show)
 data Bernoulli a = Bernoulli Double a a
 
 class Distribution a where
-  sumProbabilities :: a -> Double
+  sumProbabilities :: a b -> Double
 
-instance Distribution (Bernoulli a) where
-    sumProbabilities (Bernoulli p s f) = p + (1 - p)
-
-class Discrete a where
+class Distribution a => Discrete a where
   pmf :: Eq b => a b -> b -> Double
 
+instance Distribution Bernoulli where
+    sumProbabilities (Bernoulli p s f) = p + (1 - p)
+  
 instance Discrete Bernoulli where
-   pmf (Bernoulli p s f) x = if (x == s) then p else 1 - p
+  pmf (Bernoulli p s f) x = if (x == s) then p else 1 - p
+  
+
+
