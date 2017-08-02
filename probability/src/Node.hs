603685269cdec0321698d759c3ee1913ca4bb4f1
Dhruv Rajan <dhruv@krishnaprem.com>
2017-08-02 16:29:59 -0700

    new payload representation; localProbability function for Node CPD

--- probability/src/Node.hs
+++ probability/src/Node.hs
@@ -3,12 +3,11 @@
 module Node where
 
 import Prelude hiding (id)
-import qualified  Distribution as Dist
 import qualified Data.Map.Strict as Map
-import qualified Distribution as Dist
 
-data Payload = Bernoulli Double | If deriving (Show)
---data Payload a = Normal a a | Bernoulli Double a a deriving (Show)
+data Dist = Bernoulli Double | CPD1 Double Double deriving (Show)
+data Payload = Observed Dist Bool | Unobserved Dist deriving (Show)
+--data Payload a = Normal a a | Bernoulli Double a a deriving (Show) 
 
 
 data Node = Node 
@@ -25,4 +24,12 @@ createNode = Node
 createDetachedNode :: Payload -> Node
 createDetachedNode payload = Node 0 [] [] payload
 
-
+localProbability :: Node -> [Bool] -> Double
+localProbability node vals
+  | not $ (length vals) == 1 + (length $ parents node) = 0.0 -- TODO: Proper Error Handling
+  | otherwise = case node of
+      Node{payload=Unobserved (Bernoulli p)} -> if last vals then p else 1-p
+      Node{payload=Unobserved (CPD1 ift iff)} -> if vals !! 0 then
+                                                   if last vals then ift else 1-ift
+                                                 else
+                                                   if last vals then iff else 1 - iff
