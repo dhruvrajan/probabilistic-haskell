b92caf3e0bd815d4b0f80facf0034d7ce28d6b20
Dhruv Rajan <dhruv@krishnaprem.com>
2017-08-03 17:26:32 -0700

    fixing variable elimination code

--- probability/src/VariableElimination.hs
+++ probability/src/VariableElimination.hs
@@ -6,13 +6,12 @@ import Numeric.LinearAlgebra.HMatrix as HM
 import Data.Maybe
 import qualified Data.Map.Strict as Map
 import Data.List
+import Debug.Trace
 
-data Factor = Factor [Int] (Map.Map [Bool] Double) deriving (Show)
+-- | Factor Data Structure, representing a discrete, finite CPD
+data Factor = Factor [Int] (Map.Map [Bool] Double) deriving (Show, Eq)
 
-boolPermutations :: Int -> [[Bool]]
-boolPermutations 0 = [[]]
-boolPermutations n = let ps = boolPermutations (n - 1)
-                      in union  [True:cs | cs <- ps] [False:cs | cs <- ps]
+-- Accessor methods for Factor
 
 keys :: Factor -> [Int]
 keys (Factor l m) = l
@@ -20,6 +19,13 @@ keys (Factor l m) = l
 table :: Factor -> (Map.Map [Bool] Double)
 table (Factor l m) = m
 
+-- | Generate all possible boolean vectors of a given size
+boolPermutations :: Int -> [[Bool]]
+boolPermutations 0 = [[]]
+boolPermutations n = let ps = boolPermutations (n - 1)
+                      in union  [True:cs | cs <- ps] [False:cs | cs <- ps]
+
+-- | Generate the local factor for a specific node
 getFactor :: State -> String -> Factor
 getFactor state name = factor where
   nodeId = getNodeId state name
@@ -62,7 +68,7 @@ pointwiseProduct f1 f2 = f3 where
   table1 = table f1
   table2 = table f2
   newKeys = reverse $ sort $ union (keys f1) (keys f2)
-  perms = boolPermutations $ length newKeys
+  perms =  boolPermutations $ length newKeys
   pairs = map ( \x -> (x, splitPerm (keys f1) (keys f2) x)) perms
   evalProbs = map (\(x, (f1_v, f2_v)) -> (x, (fromJust $ Map.lookup f1_v table1) *
                                          (fromJust $ Map.lookup f2_v table2))) pairs
@@ -86,13 +92,10 @@ evalProbs = map (\(x, (f1_v, f2_v)) -> (x, (fromJust $ Map.lookup f1_v table1) *
 
 table3 = Map.fromList evalProbs
 f3 = Factor newKeys table3
-      
-  
-  
 
 sumOut :: Int -> Factor -> Factor
 sumOut nodeId (Factor vars table)  = normalizeFactor $ Factor newVars newKeys where
-  targetIdx = fromJust $ elemIndex nodeId vars
+  targetIdx = traceShowId $ fromJust $ elemIndex nodeId vars
   -- Target is True
   isTrue = Map.filterWithKey (\k _ -> k !! targetIdx == True) table
   isTrue' = Map.mapKeys (\k1 -> deleteAt targetIdx k1) isTrue
@@ -108,31 +111,31 @@ sumOut nodeId (Factor vars table)  = normalizeFactor $ Factor newVars newKeys wh
   newVars = delete nodeId vars
 
 
-s = initialState 0
-(a, s') = addBernoulli s "a" 0.3
-(b, s'') = addCPD s' "b" "a" 0.4 0.5
+-- s = initialState 0
+-- (a, s') = addBernoulli s "a" 0.3
+-- (b, s'') = addCPD s' "b" "a" 0.4 0.5
 
 
-factor = getFactor s'' "b"
-nodeId = 1
+-- factor = getFactor s'' "b"
+-- nodeId = 1
 
-vars = keys factor
-t = table factor
+-- vars = keys factor
+-- t = table factor
 
-targetIdx = fromJust $ elemIndex nodeId vars
- -- Target is True
-isTrue = Map.filterWithKey (\k _ -> k !! targetIdx == True) t
-isTrue' = Map.mapKeys (\k1 -> deleteAt targetIdx k1) isTrue
+-- targetIdx = fromJust $ elemIndex nodeId vars
+--  -- Target is True
+-- isTrue = Map.filterWithKey (\k _ -> k !! targetIdx == True) t
+-- isTrue' = Map.mapKeys (\k1 -> deleteAt targetIdx k1) isTrue
 
--- Target is False
-isFalse = Map.filterWithKey (\k _ -> k !! targetIdx == False) t
-isFalse' = Map.mapKeys (\k1 -> deleteAt targetIdx k1) isFalse
+-- -- Target is False
+-- isFalse = Map.filterWithKey (\k _ -> k !! targetIdx == False) t
+-- isFalse' = Map.mapKeys (\k1 -> deleteAt targetIdx k1) isFalse
 
--- TODO: Proper Maybe Handling
-newValues = Map.mapWithKey (\k v -> v + (fromJust $ Map.lookup k isFalse')) isTrue'
+-- -- TODO: Proper Maybe Handling
+-- newValues = Map.mapWithKey (\k v -> v + (fromJust $ Map.lookup k isFalse')) isTrue'
 
-newVars = delete nodeId vars
+-- newVars = delete nodeId vars
 
-normalizedValues = normalizeFactor $ Factor newVars newValues
+-- normalizedValues = normalizeFactor $ Factor newVars newValues
 
   
