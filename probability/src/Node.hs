cb2ee6cac10bda8d10a4fdf5e51378d964619f58
Dhruv Rajan <dhruv@krishnaprem.com>
2017-08-13 17:20:31 -0700

    removed commented-out code

--- probability/src/Node.hs
+++ probability/src/Node.hs
@@ -1,13 +1,10 @@
-{-# LANGUAGE ExistentialQuantification #-}
-
 module Node where
 
-import Prelude hiding (id)
+import Prelude
 import qualified Data.Map.Strict as Map
 
 data Dist = Bernoulli Double | CPD1 Double Double | CPD2 Double Double Double Double deriving (Show)
 data Payload = Observed Dist Bool | Unobserved Dist deriving (Show)
-
 
 data Node = Node 
   {
@@ -28,17 +25,14 @@ localProbability node vals
   | not $ (length vals) == 1 + (length $ parents node) = 0.0 -- TODO: Proper Error Handling
   | otherwise = case node of
       Node{payload=Unobserved (Bernoulli p)} -> if last vals then p else 1-p
+      Node{payload=Observed (Bernoulli p) val} -> if last vals then p else 1-p
+      
       Node{payload=Unobserved (CPD1 ift iff)} -> if vals !! 0 then
                                                    if last vals then ift else 1-ift
                                                  else
                                                    if last vals then iff else 1 - iff
-
--- localProbability :: Node -> [Bool] -> Bool -> Double
--- localProbability node ps val
---   | not $ (length ps) == (length $ parents node) = 0.0 -- TODO: Proper Error Handling
---   | otherwise = case node of
---       Node{payload=Unobserved (Bernoulli p)} -> if val then p else 1-p
---       Node{payload=Unobserved (CPD1 ift iff)} -> if ps !! 0 then
---                                                    if val then ift else 1-ift
---                                                  else
---                                                    if val then iff else 1 - iff
+      Node{payload=Observed (CPD1 ift iff) val} -> if vals !! 0 then
+                                                   if last vals then ift else 1-ift
+                                                 else
+                                                   if last vals then iff else 1 - iff                                             
+                                                   
